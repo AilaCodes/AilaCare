c93127e481844a8c3dbc5c7ea1d4b7dc24b416ea
AilaCodes <ailacodes1@gmail.com>
2025-02-03 00:13:44 +0300

    commiting improved backend

--- frontend/src/app.js
+++ frontend/src/app.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import HomePage from './pages/HomePage';
+import NavigationBar from './components/Navbar';
+import Footer from './components/Footer';
+import Status from './status';
 
 const App = () => {
   return (
-    <div>
-      <HomePage />
+    <div className="d-flex flex-column min-vh-100">
+      <NavigationBar />
+      <div className="flex-grow-1">
+        <HomePage />
+        <Status />
+      </div>
+      <Footer />
     </div>
   );
 };
